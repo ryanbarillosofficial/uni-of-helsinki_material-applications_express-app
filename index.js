104dd790cb9adfda2a7c97d9629a9bf412876766
ryanbarillosofficial <129530399+ryanbarillosofficial@users.noreply.github.com>
2026-06-08 15:41:00 -0600

    Updated index

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-	res.send("<h1>Hello Node.js!!!</h1>");
+	res.send(`<h1>Hello Node.js!!!</h1>
+<p>Hello world</p>`);
 });
 
 const PORT = 8000;
